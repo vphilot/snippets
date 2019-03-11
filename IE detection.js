8b850818ac25ae770c4f7dbd192dfebf6e0bb610
Vinicius Philot <vphilot@gmail.com>
2019-03-11 10:24:07 -0400

    Create IE detection.js

---
+++ IE detection.js
@@ -0,0 +1,6 @@
+function isIE() {
+  var ua = window.navigator.userAgent; //Check the userAgent property of the window.navigator object
+  var msie = ua.indexOf('MSIE '); // IE 10 or older
+  var trident = ua.indexOf('Trident/'); //IE 11
+  return (msie > 0 || trident > 0);
+}
